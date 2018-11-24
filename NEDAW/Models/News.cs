54ceba4f1ecb76f4c32e90f032d07a3f7e86dd41
Macovei Bogdan <bogdanmaco@yahoo.com>
2018-11-24 14:53:40 +0200

    Adding News and NewsCategory in Models

---
+++ NEDAW/Models/News.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NEDAW.Models
+{
+    public class News
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+
+        public int NewsCategoryId { get; set; }
+
+        public NewsCategory NewsCategory { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public int ModifiedBy { get; set; }
+
+        public DateTime ModifiedOn { get; set; }
+    }
+}
---
+++ NEDAW/Models/NewsCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NEDAW.Models
+{
+    public class NewsCategory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
